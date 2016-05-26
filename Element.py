d67d928b841f541ccd118e3216413a6d61e3d46c
drwiner <drwiner131@gmail.com>
2016-05-26 16:20:37 -0400

    initial load

---
+++ Element.py
@@ -0,0 +1,320 @@
+
+class Graph:
+	def __init__(self, Elements = set(), Edges = set(), Constraints = set()):
+		self.elements = Elements
+		self.edges = Edges;
+		self.constraints = Constraints
+		
+	def addEdge(self, edge):
+		if edge not in self.edges:
+			if edge.source in self.elements:
+				if edge.sink not in self.elements:
+					self.elements.add(edge.sink)
+				self.edges.add(edge)
+		
+	def addConstraint(self, edge):
+		if edge not in self.constraints:
+			if edge.source in self.elements:
+				if edge.sink not in self.elements:
+					self.elements.add(edge.sink)
+				self.constraints.add(edge)
+			else:
+				print('constraint source not in graph')
+	
+			
+	def getIncidentEdges(self, element):
+		return {edge for edge in self.edges if edge.source is element}
+	def getNeighbors(self, element):
+		return set(edge.sink for edge in self.edges if edge.source is element)
+	def getParents(self, element):
+		return set(edge.source for edge in self.edges if edge.sink is element)
+	def getNeighborsByLabel(self, element, label):
+		return set(edge.sink for edge in self.edges if edge.source is element and edge.label is label)
+	def getIncidentEdgesByLabel(self, element, label):
+		return set(edge for edge in self.edges if edge.source is element and edge.label is label)
+	def getParentsByLabel(self, element, label):
+		return set(edge.source for edge in self.edges if edge.sink is element and edge.label is label)
+	def getConstraints(self, element):
+		return {edge for edge in self.constraints if edge.source is element}
+	def getConstraintsByLabel(self, element, label):
+		return set(edge for edge in self.constraints if edge.source is element and edge.label is label)
+	def getConstraintsByParent(self, element):
+		return {edge.source for edge in self.constraints if edge.sink is element}
+		
+	def findConsistentElement(self, element):
+		return set(member for member in self.elements if member.isConsistent(element))
+		
+	#Given edge, find all other edges with same source
+	def findCoIncidentEdges(self, lonely_edge):
+		return set(edge for edge in self.edges if edge.source is lonely_edge.source and edge is not lonely_edge)
+		
+	def findConsistentEdge(self, edge):
+		return set(member for member in self.edges if member.isConsistent(edge))
+		
+	def findConsistentEdgeWithIgnoreList(self, edge, Ignore_List):
+		return set(member for member in self.edges if member not in Ignore_List and member.isConsistent(edge))
+		
+	def rGetDescendants(self, element, Descendants = set()):
+	
+		#Base Case
+		incidentEdges = self.getIncidentEdges(element)
+		if len(incidentEdges) == 0:
+			return element
+			
+		#Induction
+		for edge in incidentEdges:
+			Descendants.add(element)
+			Descendants = self.rGetDescendants(edge.sink, Descendants)
+		return Descendants
+	
+	def rGetDescendantEdges(self, element, Descendant_Edges = set()):
+		#Base Case
+		incident_Edges = self.getIncidentEdges(element)
+		if len(incident_Edges) == 0:
+			return Descendant_Edges
+		
+		#Induction
+		Descendant_Edges=Descendant_Edges.union(incident_Edges)
+		for edge in incident_Edges:
+			Descendant_Edges = self.rGetDescendantEdges(edge.sink, Descendant_Edges)
+			
+		return Descendant_Edges
+			
+		
+	def equivalentWithConstraints(self, other):
+		for c in other.constraints:
+			#First, narrow down edges to just those which are equivalent with constraint source
+			suspects = {edge.source for edge in self.edges if edge.source.isEquivalent(c.source)}
+			for suspect in suspects:
+				print('suspect id: ', suspect.id)
+				if self.constraintEquivalentWithElement(other, suspect, c.source):
+					return True
+		return False
+		
+		
+	def constraintEquivalentWithElement(self, other, self_element, constraint_element):
+		""" Returns True if element and constraint have equivalent descendant edge graphs"""
+		#Assumes self_element is equivalent with constraint_element, but just in case
+		if not self_element.isEquivalent(constraint_element):
+			return False
+		
+		descendant_edges = self.rGetDescendantEdges(self_element)
+		constraints = other.rGetDescendantConstraints(constraint_element)
+		return rDetectEquivalentEdgeGraph(constraints, descendant_edges)
+		
+		
+	def rGetDescendantConstraints(self, constraint_source, Descendant_Constraints = set()):
+		#Base Case
+		incident_constraints = self.getConstraints(constraint_source)
+		if len(incident_constraints) == 0:
+			return Descendant_Constraints
+		
+		#Induction
+		Descendant_Constraints=Descendant_Constraints.union(incident_constraints)
+		for c in incident_constraints:
+			Descendant_Constraints = self.rGetDescendantConstraints(c.sink, Descendant_Constraints)
+			
+		return Descendant_Constraints
+	
+	###############################################################
+	def isConsistent(self, other):
+		""" Returns True if for every edge in other_graph, there is some consistent edge in self"""
+		if rDetectConsistentEdgeGraph(Remaining = other.edges, Available = self.edges):
+			print('consistent without constraints')
+			if not self.equivalentWithConstraints(other):
+				print('consistent with constraints')
+				return True
+		return False
+	###############################################################
+		
+	def elementsAreConsistent(self, other, self_element, other_element):
+		if not self_element.isConsistent(other_element):
+			return False
+			
+		descendant_edges = self.rGetDescendantEdges(self_element)
+		other_descendant_edges = other.rGetDescendantEdges(other_element)
+		if rDetectConsistentEdgeGraph(other_descendant_edges,descendant_edges):
+			return True
+			
+		return False
+		
+	def elementsAreEquivalent(self, other, self_element, other_element):
+		""" Elements in different graphs (self vs other) are equivalent 
+		if edge path from self_element has superset of edge path from other_element"""
+		if not self_element.isEquivalent(other_element):
+			return False
+		
+		descendant_edges = self.rGetDescendantEdges(self_element)
+		other_descendant_edges = other.rGetDescendantEdges(other_element)
+		return rDetectEquivalentEdgeGraph(other_descendant_edges, descendant_edges)
+		
+def rDetectConsistentEdgeGraph(Remaining = set(), Available = set()):
+	""" Returns True if all remaining edges can be assigned an equivalent non-used edge in self """
+	if len(Remaining)  == 0:
+		return True
+		
+	#No solution if there are more edges remaining then there are available edges
+	if len(Remaining) > len(Available):
+		return False
+
+	other_edge = Remaining.pop()
+	print('remaining ', len(Remaining))
+	for prospect in Available:
+		if prospect.isConsistent(other_edge):
+			A = {item for item in Available if not (item is prospect)}
+			if (rDetectConsistentEdgeGraph(Remaining, A)):
+				return True
+	return False
+	
+def rDetectEquivalentEdgeGraph(Remaining = set(), Available = set()):
+	""" Returns True if all remaining edges can be assigned an equivalent non-used edge in self """
+	if len(Remaining)  == 0:
+		return True
+		
+	#No solution if there are more edges remaining then there are available edges
+	if len(Remaining) > len(Available):
+		return False
+
+	other_edge = Remaining.pop()
+	print('constraints remaining ', len(Remaining))
+	for prospect in Available:
+		if prospect.isEquivalent(other_edge):
+			A = {item for item in Available if not (item is prospect)}
+			if (rDetectEquivalentEdgeGraph(Remaining, A)):
+				return True
+	return False
+
+		
+class Edge:
+	def __init__(self, source, sink, label):
+		self.source=  source
+		self.sink = sink
+		self.label = label
+		
+	def isProperty(self,other,property):
+		return self.property(other)
+		
+	def isConsistent(self, other):
+		if self.source.isConsistent(other.source) and self.sink.isConsistent(other.sink) and self.label == other.label:
+			return True
+		return False
+
+	def isEquivalent(self, other):
+		if self.source.isEquivalent(other.source) and self.sink.isEquivalent(other.sink) and self.label == other.label:
+			return True
+		return False
+		
+class Element:
+	def __init__(self, id, type = None, name = None):
+		self.id = id
+		self.type = type
+		self.name = name
+		
+	def isConsistent(self, other):
+		#""" Returns True if self and other have same name or unassigned"""
+		if not other.type is None:
+			if self.type != other.type:
+				return False
+		return True
+		
+	def isIdentical(self, other):
+		if self.id == other.id:
+			if self.isEquivalent(other):
+				return True
+		return False
+		
+	def isEquivalent(self, other):
+		if self.type == other.type:
+			return True
+		return False
+	
+	def isProperty(self, other, property):
+		return self.property(other)
+		
+			
+		
+class InternalElement(Element):
+	def __init__(self, id, type, name = None, num_args = 0):
+		super(InternalElement,self).__init__(id,type,name)
+		self.num_args = num_args
+		
+	# def isEquivalent(self, other):
+		# if self.type == other.type and self.name == other.name and self.num_args == other.num_args:
+			# return True
+		# return False
+	
+	def isEquivalent(self, other):
+		# for each incident edge of other, there is a unique equivalent edge of self
+		# and for each constraint of other, there is no uniqe equivalent edge of self
+		if not (super(InternalElement,self).isEquivalent(other) \
+			and self.name == other.name \
+			and self.num_args == other.num_args):
+			return False
+			
+		#out_incident_edges = {edge for edge in }
+	
+	def isConsistent(self, other):
+	
+		#If other has a type, then return False if they are not the same
+		if not super(InternalElement,self).isConsistent(other):
+			return False
+				
+		#If other has an argument number, then return False if they are not the same
+		if other.num_args > 0:
+			if self.num_args != other.num_args:
+				return False
+	
+		#If other has a predicate name, then return False if they are not the same
+		if not other.name is None:
+			if self.name != other.name:
+				return False
+				
+		return True
+		
+				
+		
+class Operator(InternalElement):
+	def __init__(self, id, type, name = None, num_args = 0, executed = None):
+		super(Operator,self).__init__(id,type,name, num_args)
+		self.executed = executed
+		
+		
+class Literal(InternalElement):
+	def __init__(self, id, type, name = None, num_args = 0, truth = None):
+		super(Literal,self).__init__(id,type,name, num_args)
+		self.truth = truth
+
+	def isConsistent(self, other):
+		if not super(Literal,self).isConsistent(other):
+			return False
+				
+		if not other.truth is None:
+			if self.truth != other.truth:
+				return False
+				
+		return True
+
+	def isEquivalent(self,other):
+		if self.name == other.name:
+			return True
+		return False
+		
+
+		
+class Argument(Element):
+	def __init__(self, id, type, name= None, position = 0):
+		super(Argument,self).__init__(id,type,name)
+		self.position = position
+		
+	def isConsistent(self, other):
+		if not super(Argument,self).isConsistent(other):
+			return False
+		if other.position > 0 and other.position != self.position:
+			return False
+		return True
+	
+	def isEquivalent(self, other):
+		if super(Argument,self).isEquivalent(other) and self.position == other.position:
+			return True
+		return False
+	
---
+++ testing_elements.py
@@ -0,0 +1,71 @@
+from Element import *
+
+excavate = Operator(id = 1, type = 'op', name = 'excavate', num_args = 3)
+p1 = Literal(id=2, type='precondition', name= 'alive', num_args = 1, truth = True)
+p2 = Literal(id=3, type='precondition', name= 'at', num_args = 2, truth = True)
+p3 = Literal(id=4, type='precondition', name= 'burried', num_args = 2, truth = True)
+p4 = Literal(id=5, type='precondition', name= 'knows-location', num_args = 3, truth = True)
+
+e1 = Literal(id=6, type='effect', name='burried', num_args=2, truth = False)
+e2 = Literal(id=7, type='effect', name='has', num_args=2, truth = True)
+
+consent = Argument(id=8, type='actor', name='?character', position = 1)
+item = Argument(id = 9, type='var', name='?item', position=2)
+place = Argument(id = 10, type='var', name='?place', position=3)
+
+edge0 = Edge(excavate, consent, 'actor-of')
+edge1 = Edge(excavate, p1, 'precond-of')
+edge2 = Edge(excavate, p2, 'precond-of')
+edge3 = Edge(excavate, p3, 'precond-of')
+edge4 = Edge(excavate, p4, 'precond-of')
+
+edge5 = Edge(excavate, e1, 'effect-of')
+edge6 = Edge(excavate, e2, 'effect-of')
+
+edge7 = Edge(p1, consent, 'first-arg')
+edge8 = Edge(p2, consent, 'first-arg')
+edge9 = Edge(p4, consent, 'first-arg')
+edge10 = Edge(e2, consent, 'first-arg')
+edge11 = Edge(excavate, consent, 'first-arg')
+
+edge12 = Edge(p2, item, 'sec-arg')
+edge13 = Edge(p3, item, 'first-arg')
+edge14 = Edge(p4, item, 'sec-arg')
+edge15 = Edge(e1, item, 'first-arg')
+edge16 = Edge(e2, item, 'sec-arg')
+
+edge17 = Edge(p2, place, 'sec-arg')
+edge18 = Edge(p3, place, 'sec-arg')
+edge19 = Edge(p4, place, 'third-arg')
+edge20 = Edge(e1, place, 'sec-arg')
+
+excavate_elements = {excavate, p1, p2, p3, p4, e1, e2, consent, item, place}
+excavate_edges = {edge0, edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11, edge12, edge13, edge14,edge15, edge16,edge17, edge18, edge19, edge20}
+
+Excavate_graph = Graph(excavate_elements, excavate_edges)
+
+example = Operator(id = 1, type= 'op')
+example_p1 = Literal(id=2, type='precondition', name='alive', truth = True)
+example_e1 = Literal(id=3, type = 'effect', name='has', truth = True)
+example_actor = Argument(id=4, type='actor', position=1)
+example_item = Argument(id=5,type='var', position=2)
+
+example_edge0 = Edge(example, example_p1, 'precond-of')
+example_edge1 = Edge(example, example_e1, 'effect-of')
+example_edge2 = Edge(example_p1, example_actor, 'first-arg')
+example_edge3 = Edge(example_e1, example_actor, 'first-arg')
+example_edge4 = Edge(example_e1, example_item, 'sec-arg')
+
+#Cannot have a precondition with name 'knows-location', first-arg is actor, sec-arg is item
+#Since we know this is precondition of excavate, the example_graph should not be consistent
+example_constraint_Literal = Literal(id=6, type ='precondition', name='knows-location', truth = True)
+example_constraint_edge0 = Edge(example_constraint_Literal, example_actor, 'first-arg')
+example_constraint_edge1 = Edge(example_constraint_Literal, example_item, 'sec-arg')
+
+example_elements = {example, example_p1, example_e1, example_actor, example_item,example_constraint_Literal}
+example_edges = {example_edge0, example_edge1, example_edge2, example_edge3, example_edge4}
+example_constraints = {example_constraint_edge0, example_constraint_edge1}
+
+Example_graph = Graph(example_elements,example_edges, example_constraints)
+
+print(Excavate_graph.isConsistent(Example_graph))
