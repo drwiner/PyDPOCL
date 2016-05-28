2d540eb1db82ea76d5758332464261314c7468c8
drwiner <drwiner131@gmail.com>
2016-05-27 22:13:28 -0400

    literals to conditions, graphs to subgraphs

--- Element.py
+++ Element.py
@@ -90,11 +90,20 @@ class Literal(InternalElement):
 				
 		return True
 
-	def isEquivalent(self,other):
-		if self.name == other.name:
+	def isCoConsistent(self, other):
+		if self.isConsistent(other) and other.isConsistent(self):
 			return True
 		return False
 		
+	def isEquivalent(self,other):
+		if self.name == other.name and self.type == other.type:
+			return True
+		return False
+		
+	def isEqual(self, other):
+		if self.isEquivalent(other) and other.isEquivalent(self):
+			return True
+		return False
 
 		
 class Argument(Element):
@@ -131,7 +140,12 @@ class Graph(Element):
 	def addConstraint(self, edge):
 		if edge not in self.constraints:
 			self.constraints.add(edge)
-	
+			
+	def merge(self, other):
+		#Danger of a union is that we'll represent consistent elements twice.
+		#check if two steps with nonempty intersection can be merged
+		#For just literals first:
+		if 
 			
 	def getIncidentEdges(self, element):
 		return {edge for edge in self.edges if edge.source is element}
@@ -294,46 +308,132 @@ def rDetectEquivalentEdgeGraph(Remaining = set(), Available = set()):
 			if (rDetectEquivalentEdgeGraph(Remaining, A)):
 				return True
 	return False
-
-class Belief(Element):
-	def __init__(self, id, type, name=None, story_element):
-		super(Belief, self).__init__(id,type,name)
-		self.story_element = story_element
 	
-	def isEquivalent(other_belief):
-		if self.story_element.isEquivalent(other_belief.story_element) \
-		and other_belief.story_element.isEquivalent(self.story_element):
-			return True:
-		return False
+def extractSubgraphFromElement(G, element, Type):
+	Edges = G.rGetDescendantEdges(element)
+	Elements = G.rGetDescendants(element)
+	Constraints = G.rGetDescendantConstraints(element)
+	return Type(element.id,type = 'subgraph',Elements,Edges,Constraints)
+
+class Belief(Graph):
+	def __init__(self, id, type, name=None, Elements=set(), Edges = set(), Constraints = set()):
+		super(Belief, self).__init__(Elements, Edges, Constraints)
+		self.id = id
+		self.type= type
+		self.name = name
+	
+	# def isEquivalent(other_belief):
+		# if self.story_element.isEquivalent(other_belief.story_element) \
+		# and other_belief.story_element.isEquivalent(self.story_element):
+			# return True:
+		# return False
 		
-class Action(Element):
-	def __init__(self, id, type, name=None):
-		super(Action,self).__init__(id,type,name)
-		
-class CausalLink(Edge):
-	def __init__(self, id, type, name=None, action1, action2, condition):
-		super(CausalLink,self).__init__(source=action1,sink=action2,label=condition)
+class Action(Graph):
+	""" Action Graph: for step graph"""
+	def __init__(self, id, type, name=None, Elements=set(), Edges=set(), Constraints = set()):
+		super(Action,self).__init__(Elements,Edges,Constraints)
 		self.id = id
 		self.type = type
 		self.name = name
+		
+	"""Determine if two Actions can be joined by a causal link"""
+	def isConsistentAntecedentFor(self, action):
+		effects = {egde.sink for edge in self.edges if edge.label == 'effect-of'}
+		if len(effects) == 0:
+			return False
+		preconditions = {edge.sink for edge in self.edges if edge.label == 'precond-of'}
+		if len(preconditions) == 0:
+			return False
+		prospects = {(eff,pre) for eff in effects for pre in preconditions if eff.isCoConsistent(pre)}
+		if len(prospects)  == 0:
+			return False
+		return prospects
+	
+		
+class Condition(Graph):
+	""" A Literal used in causal link"""
+	def __init__(self,id,type,name=None,Elements=set(), Edges = set(), Constraints = set()):
+		super(Condition,self).__init__(Elements,Edges,Constraints)
+		self.id = id
+		self.type = type
+		self.name  name
+		
+	def merge(self, other):
+		#Do not assume equivalent, assume only consistency
+		if not self.isEqual(other):
+			if not self.isConsistent(other) and other.isConsistent(self):
+				print('could not merge conditions')
+				return False
+			else:
+				
+		
+def literalsToCondition(G1, G2, literal_1, literal_2, checked=False):
+	if not checked and not literal_1.isCoConsistent(literal_2):
+		return False
+	
+	if literal_1.name is None:
+		name = literal_2.name
+	else:
+		name = literal_1.name
+		
+	
+	if literal_1.num_args is None:
+		if literal_2.num_args is None:
+			print('need to put num_args in literals')
+		else:
+			num_args = literal_2
+	else:
+		num_args = literal_1.num_args
+	
+	#get elements and edges recursively from literal_1 in G_1
+	L1 = extractSubgraphFromElement(G1,literal_1,Literal)
+	L2 = extractSubgraphFromElement(G2,literal_2,Literal)
+	
+	edge 
+	{edge for edge in L1.edges if edge.label is 'first-arg'}
+	L1.merge(L2)
+	return Condition(id=literal_1.id, type='condition')
+	
+	
+		
+class CausalLink(Edge):
+	""" A causal link is an edge, s.t. the source and sink are actions, and the condition is itself an edge between a dummy element
+		and the dependency literal element, rather than just a label"""
+	def __init__(self, id, type, name=None, action1, action2, condition):
+		super(CausalLink,self).__init__(source=action1,sink=action2,label=condition.id)
+		self.id = id
+		self.type = type
+		self.name = name
+		self.dependency = condition #A literal element#
+		
+	def isInternallyConsistent(self):
+		effects = {egde.sink for edge in self.edges if edge.label == 'effect-of' and edge.sink.id == self.condition.id}
+		if len(effects) == 0:
+			return False
+		preconditions = {edge.sink for edge in self.edges if edge.label == 'precond-of' and edge.sink.id == self.condition.id}
+		if len(preconditions) == 0:
+			return False
+		return True
+		
 	
 	#def possiblyThreatenedBy(self,action):
 	
 	def threatenedBy(self,action):
 		action_orderings = getOrderingsWith(action)
 		
+#Do I need this?
 class Binding(Edge):
-	def __init__(element1, element2, binding_type):
+	def __init__(self, id, type, name=None, element1, element2, binding_type):
 		super(Binding,self).__init__(element1,element2,binding_type)
 		self.X = element1
 		self.Y = element2
+		self.id = id
+		self.type = type
+		self.name = name
 		
-	def isInternallyConsistent(self, element_A, element_B):
-		if self.X.isIdentical(element_A) and self.Y.isIdentical(element_B):
+	def isInternallyConsistent(self):
+		if self.source.isIdentical(self.sink):
 			return True
-		if self.Y.isIdentical(element_B) and self.X.isIdentical(element_A):
-			return True
-		return False
 		
 class Ordering(Edge):
 	def __init__(self,action1,action2):
@@ -399,3 +499,32 @@ class IntentionFrame(Element):
 		
 		return True
 		
+		
+def DomainOperator(Graph):
+	def __init__(self,id,type,name=None, Elements, Operator_Element, Edges,Constraints):
+		super(DomainOperator,self).__init__(id,type,name,Elements,Edges,Constraints)
+		self.Operator = Operator_Element
+		Args = {i:arg for i in range(self.Operator.num_args) for arg in self.Elements if type(arg) is Argument and arg.arg_pos_dic[i]}
+		
+def Prerequisite(Graph):
+	def __init__(self,id,type,name=None, Elements, Edges, Constraints):
+		super(Prerequisite,self).__init__(id,type,name,Elements,Edges,Constraints)
+		Causal_Links = {edge for edge in Edges if type(edge) is CausalLink}
+		Orderings = {edge for edge in Edges if type(edge) is Ordering}
+		Steps = {element for element in Elements if type(element) is Operator}
+	
+	def evaluateOperators(self,Operators):
+		self.consistent_mappings = {step.id : {D.id for D in Operators if D.isConsistent(step)} for step in Steps}
+		#TODO: check if two steps with nonempty intersection can be merged
+		
+		#{self.consistent_mappings[step.id].add(D.id) for step in Steps for D in Operators if D.isConsistent(step)}	
+		#for D in Operators:
+		#	{self.consistent_mappings[step.id].add(D.id) for step in Steps if D.isConsistent(step)}
+			
+	def evaluateCausalLinks(self)
+		""" For every pair of steps, determine if consistent for causal link. 
+			Create a "condition" by finding an source.effect that is consistent with a sink.precondition"""
+		"""For every causal link Qi --Condition()--> Qj, find steps di = c-map[Qi.id] and dj = c-map[Qj.id] s.t. 
+			Condition().Args[di.id] == Condition().Args[dj.id]
+			For e"""
+		{cl.id: {}}
