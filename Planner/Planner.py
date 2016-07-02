fee6ede1a85295f4d7406c67f3d95172912cb152
drwiner <drwiner131@gmail.com>
2016-07-02 11:27:21 -0400

    adding planspaceplanner notes and file

---
+++ Planner/Planner.py
@@ -0,0 +1,41 @@
+from Flaws import *
+
+"""
+	Algorithm for Plan-Space search of Story Plan
+"""
+
+"""
+	(1) Read PDDL Domain and Problem (in another file, make test case)
+	(2) Create dummy initial and goal steps
+	(3) Create open precondition flaws for each element in goal
+	(4) Select Flaw based on heuristic
+	(5) Support the following operations pertaining to resolving open precondition flaws:
+		(5.A) Determine if an operator graph has an effect which is consistent with precondition in flaw
+		(5.B) Determine if an existing step has an effect which is consistent with precondition in flaw (if there is no ordering path from s_need to s_new)
+	(6) Support the following operations pertaining to resolving a threatened causal link flaw:
+		(6.A) Trivially, adding ordering edges
+		(6.B) Not as trivially, add bindings to prevent effect from co-designating with precondition.
+					Transform effect in E1 (condition) and preconditino in P1 (condition)
+							[Quick reference: 	arguments are named only if they refer to constants
+												arguments which are co-designated have the same ID
+												TODO: for each argument, track a set of IDs for non-designations]
+					For each matching outgoing-labeled edge sinks of E1 and P1, call them eA and pA, 
+							skip if they are codesignated or assigned the same name
+							otherwise, create deep copy of graph and add a non-codesignation relation
+							search through all arguments to create graphs which prevent unification in every possible way
+	(7) Detect Threatened Causal Link Flaws
+	(8) Recursive Invocation (but, log status of plan first)
+"""
+
+class PlanSpacePlanner:
+	flaws = [] #TODO, sorted by heuristic value
+	def __init__(self, start_set, end_set, op_graphs, objects):
+		#Assumes these parameters are already read from file
+		self.start_state = start_set
+		self.end_state = end_set
+		self.createDummy(start_set, end_set)
+		
+	
+	def createDummy(self, start_set, end_set):
+		#create
+		pass
