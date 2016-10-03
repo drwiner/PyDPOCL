ccd3407beaf08c21866e098a2a7cd2787a199bb0
David <drwiner@ncsu.edu>
2016-10-02 22:52:39 -0400

    small meaningless edit

--- pddlToGraphs.py
+++ pddlToGraphs.py
@@ -30,10 +30,10 @@ def makeGoal(formula, current_id):
 	if formula.key == 'not':
 		formula = next(iter(formula.children))
 		num_children = len(formula.children)
-		lit = Literal(ID = uuid.uuid1(current_id), typ = 'Condition', name = formula.key, num_args = num_children, truth = False)
+		lit = Literal(ID=uuid.uuid1(current_id), typ='Condition', name=formula.key, num_args=num_children, truth=False)
 	else:
 		num_children = len(formula.children)
-		lit = Literal(ID = uuid.uuid1(current_id), typ = 'Condition', name = formula.key, num_args = num_children, truth = True)
+		lit = Literal(ID=uuid.uuid1(current_id), typ='Condition', name=formula.key, num_args=num_children, truth=True)
 	return lit
 	
 def makeMotive(formula, current_id, parent, relationship, elements, edges, bit = None):
