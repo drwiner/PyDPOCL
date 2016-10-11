ca129ee2aedd35b64dfb0332e427b2b3e3c1aae9
drwiner <drwiner131@gmail.com>
2016-10-10 21:09:49 -0400

    Narrow in on goal 115

--- Planner.py
+++ Planner.py
@@ -276,6 +276,8 @@ class PlanSpacePlanner:
 		elif flaw.name == 'dcf':
 			print(GL[flaw.flaw].name)
 			story = other.deepcopy()
+			if 115 in {s.stepnumber for s in plan.D.Steps}:
+				print('stop here for tests')
 			results = story.Unify(GL[flaw.flaw].ground_subplan.deepcopy(), self.story_GL)
 			print(len(results))
 			GL = self.story_GL
